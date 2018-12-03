2ab2098ef3689a8340066ea4d59ac7559debc0ab
Chase Hausman <chase.hausman@comcast.net>
2018-12-02 22:38:04 -0800

    Week 6 Submission

---
+++ InventoryManager.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milestone_Project
+{
+    class InventoryManager
+    {
+        private Item[] Items;
+
+        public InventoryManager()
+        {
+            this.Items = new Item[0];
+        }
+
+        public void Add(Item newItem)
+        {
+            Item[] temp = new Item[this.Items.Length + 1];
+
+            int id = 0;
+            foreach(Item item in this.Items)
+            {
+                temp[id] = this.Items[id];
+                id++;
+            }
+
+            temp[id] = newItem;
+
+            this.Items = temp;
+        }
+
+        public void Remove(Item RemovedItem)
+        {
+            Item[] temp = new Item[this.Items.Length - 1];
+            int id = 0;
+            foreach(Item item in this.Items)
+            {
+                if(item.GetId() != RemovedItem.GetId())
+                {
+                    temp[id] = item;
+                }
+
+                id++;
+            }
+
+            this.Items = temp;
+        }
+
+        public int Restock(Item Item, int Quantity)
+        {
+            for(int i = 0; i < this.Items.Length; i++)
+            {
+                if(this.Items[i].GetId() == Item.GetId())
+                {
+                    return this.Items[i].RecieveInventory(Quantity);
+                }
+            }
+
+            return 0;
+        }
+
+        public Item[] Display()
+        {
+            foreach(Item i in this.Items)
+            {
+                Console.WriteLine(i.GetDescription() + ": $" + i.GetPrice()+" ("+i.GetStock()+" available)");
+            }
+
+            return this.Items;
+        }
+
+        public Item[] Search(String name)
+        {
+            List<Item> temp = new List<Item>();
+
+            foreach(Item i in this.Items)
+            {
+                if(i.GetDescription().Contains(name))
+                {
+                    temp.Add(i);
+                }
+            }
+
+            return temp.ToArray();
+        }
+
+        public Item[] Search(Double price)
+        {
+            List<Item> temp = new List<Item>();
+
+            foreach (Item i in this.Items)
+            {
+                if (i.GetPrice().Equals(price))
+                {
+                    temp.Add(i);
+                }
+            }
+
+            return temp.ToArray();
+        }
+    }
+}
---
+++ InventoryManagerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milestone_Project
+{
+    class InventoryManagerTests
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Creating a new inventory manager and adding new items.");
+
+            InventoryManager manager = new InventoryManager();
+            Item testItem1 = new Item(1,"Stuff",10.51,16,10,"Large",null,"Stuffs");
+            Item testItem2 = new Item(2,"Things", 15.10,22.2,11,"Small",null,"Stuffs");
+            Item testItem3 = new Item(3, "Dohickies", 7.25, 11.5, 1, "Small", null, "Stuffs");
+            manager.Add(testItem1);
+            manager.Add(testItem2);
+            manager.Add(testItem3);
+
+            Console.WriteLine("Displaying Items.");
+            manager.Display();
+
+            Console.WriteLine("Removing an item.");
+            manager.Remove(testItem3);
+            manager.Display();
+
+            Console.WriteLine("Restocking an item.");
+            manager.Restock(testItem2, 10);
+            manager.Display();
+        }
+    }
+}
